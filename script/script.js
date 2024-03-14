30edf1ce1c5d5be5211d23b00bfa0727c3561553
João Pedro <106189730+joaosilva2007@users.noreply.github.com>
2024-03-14 08:53:55 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,6 +1,29 @@
-function aperte(){
-    while(true){
-      alert("Alô");
+function abrir_aba(num){
+  for(let i= 1; i <= 5; i++){
+    let id_btn = "btn" +i;
+    let id_aba = "aba" +i;
+    document.getElementById(id_btn).disable = false;
+    document.getElementById(id_aba).style.display = "none";
+  } 
+   
+    if(num == "1"){
+    document.getElementById("btn1").disable = true;
+    document.getElementById("aba1").style.display = "block";
     }
-    
+    if(num == "2"){
+    document.getElementById("btn2").disable = true;
+    document.getElementById("aba2").style.display = "block";
+      }
+    if(num == "3"){
+    document.getElementById("btn3").disable = true;
+    document.getElementById("aba3").style.display = "block";
+        }
+    if(num == "4"){
+    document.getElementById("btn4").disable = true;
+    document.getElementById("aba4").style.display = "block";
+          }
+    if(num == "5"){
+    document.getElementById("btn5").disable = true;
+    document.getElementById("aba5").style.display = "block";
+    }
 }
